c9f5c62015cd68406d6979deb9c3313d6b7a2b66
Youssef Halleb <161064366+YoussefHalleb@users.noreply.github.com>
2026-05-04 11:20:27 +0100

    Fix formatting of cookie-parser import

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
-const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser"); 
 const pool = require("./db");
 const multer = require("multer");
 const path = require("path");
