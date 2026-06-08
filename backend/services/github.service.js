cd457c857511ea44a10a40dc72e98c8538fc116e
YoussefHalleb <youssefhalleb20@gmail.com>
2026-06-08 20:15:47 +0100

    Read GitHub token from Secret Manager

--- backend/services/github.service.js
+++ backend/services/github.service.js
@@ -1,6 +1,13 @@
 const axios = require("axios");
+const { getSecret } = require("../secretManager");
+
+async function getGithubToken() {
+  return await getSecret("github-token");
+}
 
 async function triggerPipeline({ repo_url, repo_branch, app_port }) {
+  const token = await getGithubToken();
+
   const githubUrl = `https://api.github.com/repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/actions/workflows/${process.env.GITHUB_WORKFLOW_FILE}/dispatches`;
 
   await axios.post(
@@ -15,7 +22,7 @@ async function triggerPipeline({ repo_url, repo_branch, app_port }) {
     },
     {
       headers: {
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        Authorization: `Bearer ${token}`,
         Accept: "application/vnd.github+json",
       },
       timeout: 30000,
