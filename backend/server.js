0d07f09a5d1d36c9088582ca3c263ed7bb0f739e
YoussefHalleb <youssefhalleb20@gmail.com>
2026-06-17 19:27:54 +0100

    server

--- backend/server.js
+++ backend/server.js
@@ -72,7 +72,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
 app.use(
